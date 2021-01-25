210322db6e98b69e3d4d71e2fe707f390acef7d2
Keith Marble <kmarble96@gmail.com>
2021-01-25 10:38:38 -0500

    init

---
+++ main/producer.py
@@ -0,0 +1,11 @@
+import pika, json
+
+params = pika.URLParameters('amqps://zvihcfla:Vmkzg4_yzGDL2Ldh6Dkq7gLVNhzqTTOQ@orangutan.rmq.cloudamqp.com/zvihcfla')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+def publish(method, body):
+  properties = pika.BasicProperties(method)
+  channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
